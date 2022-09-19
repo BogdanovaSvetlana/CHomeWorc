d22190f3c5fe76dc0f1b13177438680b4c81da16
BogdS <Bogdanovas85@gmail.com>
2022-09-19 20:52:09 +0300

    2семинар 1задача

---
+++ FirstApp006_Yust/Program.cs
@@ -0,0 +1,29 @@
+﻿// void Insert(int a)
+//     {
+// Console.WriteLine("Вставить" + a);
+//     }
+
+int creatNumber()
+{
+    int number = new Random().Next(10,100);
+    return number;
+}
+
+int MaxDigit(int number)
+{
+    int Decimal = number / 10;
+    int ed = number % 10;
+
+    if(Decimal >= ed)
+    {
+        return Decimal;
+    }
+    else 
+    {
+        return ed;
+    }
+}
+
+int a = creatNumber();
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a));
