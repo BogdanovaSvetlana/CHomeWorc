f0a20ef90279161843fcba30965148a16a7889ef
BogdS <Bogdanovas85@gmail.com>
2022-09-19 21:15:50 +0300

    2семинар 2задача

---
+++ HomeWork007_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿int creatNumber()
+{
+    int number = new Random().Next(100,999);
+    return number;
+}
+
+int MaxDigit(int number)
+{
+    int Sotni = number / 100;
+    int Ed = number % 10;
+    return Sotni * 10 + Ed;
+}
+
+
+int a = creatNumber();
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a));
