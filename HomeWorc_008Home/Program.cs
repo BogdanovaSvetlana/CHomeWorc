4656e28f714bca42e028a1b9a932c22a995401ad
BogdS <Bogdanovas85@gmail.com>
2022-09-20 23:08:42 +0300

    2семинар 3задача

---
+++ HomeWorc_008Home/Program.cs
@@ -0,0 +1,37 @@
+﻿// Console.WriteLine("Введите первое число!");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число!");
+
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (b % a == 0)
+// { 
+//     Console.WriteLine("Первое число кратно второму ");
+// }
+// else
+// {
+//     Console.WriteLine("Первое число не кратно второму ");
+//     // Console.WriteLine("Остаток от деления равен " + (b % a));
+//     Console.WriteLine($"Остаток от деления равен {b % a}");
+// }
+
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+void Module( int a, int b)
+{
+    if (b % a == 0)
+{ 
+    Console.WriteLine("Первое число кратно второму ");
+}
+else
+{
+    Console.WriteLine("Первое число не кратно второму ");
+    Console.WriteLine($"Остаток от деления равен {b % a}");
+}
+
+}
+
+Module(a, b);
