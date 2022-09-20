9b80c19b30d4f8d482a9cd58e87f37ec8dd7feec
BogdS <Bogdanovas85@gmail.com>
2022-09-21 02:32:12 +0300

    1задача домашка

---
+++ FirstApp009_2Seminar1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+int creatNumber()
+{
+    int number = new Random().Next(100,999);
+    return number;
+}
+
+int toNumber(int x)
+{
+    int y = x % 100;
+    int result = y / 10;
+    return result;
+}
+
+// void printNumbers()
+// {
+//    for (int i = 1; i <= 15; i++)
+//       {
+//        int b = creatNumber();
+//        Console.WriteLine($"{b}  ->  {toNumber(b)}");
+//       }
+// }
+
+
+int a = creatNumber();
+Console.WriteLine($"{a}  ->  {toNumber(a)}");
+
+
+// printNumbers();
