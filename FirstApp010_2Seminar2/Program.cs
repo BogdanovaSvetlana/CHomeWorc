9f69601ea819746698b94406f4c8e1d697975541
BogdS <Bogdanovas85@gmail.com>
2022-09-22 02:01:59 +0300

    2задача домашка

---
+++ FirstApp010_2Seminar2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+int numLen(int x)
+{
+    int i = 0;
+    while(x > 0)
+    {
+        x = x / 10;
+        i++;
+    }
+    return i;
+}
+
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int s = numLen(x);
+
+
+if(s >= 3)
+{
+    int d1 = Convert.ToInt32(Math.Pow(10, s - 2));
+    int d2 = Convert.ToInt32(Math.Pow(10, s - 3));
+    int y = x % d1;
+    int z = y / d2;
+    Console.WriteLine($"{x}  ->  {z}");
+}
+else
+{
+    Console.WriteLine($"{x}  ->  третьей цифры нет ");
+}
