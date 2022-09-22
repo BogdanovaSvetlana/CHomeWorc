73f70db4eeb3be9a5f5ac415bbb1fb04ec096cc6
BogdS <Bogdanovas85@gmail.com>
2022-09-22 21:48:52 +0300

    3семинар4

---
+++ FirstApp014_KlassWork4/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetPow(int number)
+{
+    int index = 1;
+
+    while (index <= number)
+    {
+        double a = Math.Pow(index, 2);
+        Console.WriteLine(a);
+        index++;
+    }
+}
+
+GetPow(GetNumber());
+
