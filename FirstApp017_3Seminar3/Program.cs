4647ee48bb965d209e4c7d2dd0f83ac34a8f3f6a
BogdS <Bogdanovas85@gmail.com>
2022-09-25 01:28:13 +0300

    Задача23

---
+++ FirstApp017_3Seminar3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetPow(int x)
+{
+    int index = 1;
+    Console.Write($"{x} -> ");
+
+    while (index <= x)
+    {
+        int a = index * index * index;
+        Console.Write($"{a}, ");
+        index++;
+    }
+
+}
+
+
+GetPow(GetNumber());
