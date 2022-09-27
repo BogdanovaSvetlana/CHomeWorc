f118e9fe18dc84599af6c6cb7736902a7b220daa
BogdS <Bogdanovas85@gmail.com>
2022-09-28 02:51:15 +0300

    HomeWorc27

---
+++ FirstApp024_4Seminar/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetResult(int x)
+{
+    int r = 0;
+    
+    while (x > 0)
+    {
+        int y = x % 10;
+        r = r + y;
+        x = x / 10;
+    }
+    return r;
+}
+
+int x =  GetNumber();
+GetResult(x);
+
+Console.WriteLine($"{x} ->  {GetResult(x)}");
