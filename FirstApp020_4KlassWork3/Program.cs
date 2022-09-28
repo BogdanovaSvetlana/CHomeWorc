61c33aeb890a7704ec231b1017593bba098690b2
BogdS <Bogdanovas85@gmail.com>
2022-09-28 14:59:51 +0300

    работаВклассе3

---
+++ FirstApp020_4KlassWork3/Program.cs
@@ -0,0 +1,38 @@
+﻿// SЗадача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int GetNumber() // Ручной ввод целого числа
+{
+    int num = 0;
+    while (LengthNum(num) <= 2)
+    {
+    Console.WriteLine("Введите число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+int LengthNum(int Number) // Получение количества цифр в числе
+{
+    int DigCount = 1;
+    while(Math.Abs(Number/10)>0)
+    {
+        Number = Number / 10;
+        DigCount++;
+    }
+    return DigCount;
+}
+
+int GetDigitFromNumber(int number, int index)
+{
+    int num = number/Convert.ToInt32(Math.Pow(10,LengthNum(number)-index))%10;
+    return num;
+}
+
+int num = GetNumber();
+Console.WriteLine("Вторая цифра: " + GetDigitFromNumber(num,2));
+Console.WriteLine("Третья цифра: " + GetDigitFromNumber(num,3));
+Console.WriteLine("Четвертая цифра: " + GetDigitFromNumber(num,4));
+Console.WriteLine("Пятая цифра: " + GetDigitFromNumber(num,5));
