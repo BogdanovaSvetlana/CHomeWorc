8497d21464f5de56553708c47cdc2f97b5acafb0
BogdS <Bogdanovas85@gmail.com>
2022-09-30 02:51:01 +0300

    HomeWork34

---
+++ FirstApp031_5Seminar1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+     Console.Write("[ "); // [ 1, 58, ...]
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int FindElements(int[] arr)
+{
+    int z = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            z++;
+        }
+    }
+    return z;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+int z = FindElements(array);
+Console.Write($" -> {z}");
