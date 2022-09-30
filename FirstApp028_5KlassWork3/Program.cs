9fe72cfeb99a66b623e19eb86cf17009c8857223
BogdS <Bogdanovas85@gmail.com>
2022-09-30 20:13:36 +0300

    работаВклассе3

--- FirstApp028_5KlassWork3/Program.cs
+++ FirstApp028_5KlassWork3/Program.cs
@@ -37,3 +37,41 @@ PrintArray(array);
 getProizv(array);
 PrintArray(array);
 
+
+// void FillArray(int[] array, int min, int max) // Случайный ввод массива по заданному диапазону
+// {
+//     int lenght = array.Length;
+//     for(int i = 0; i < lenght; i++)
+//     {
+//         array[i] = new Random().Next(min, max+1);
+//     }
+// }
+
+// string StringArray(int[] array, string split) // Преобразование массива в строку с заданным разделителем
+// {
+//     int lenght = array.Length;
+//     string textarray = String.Empty;
+//     for( int i = 0; i < lenght; i++)
+//     {
+//         textarray += array[i];
+//         if(i < lenght - 1)
+//         {
+//             textarray += split;
+//         }
+//     }
+//     return textarray;
+// }
+
+// void ChangeArray(int[] arr) // Замена элементов массива
+// {
+//      for( int i = 0; i < arr.Length; i++)
+//      {
+//         arr[i] *= -1;
+//      }
+// }
+
+// int[] array = new int[20];
+// FillArray(array,-9,9);
+// Console.WriteLine("Случайно заполненный массив: [" + StringArray(array, ", ") + "]");
+// ChangeArray(array);
+// Console.WriteLine("Массив с заменой элементов: [" + StringArray(array, ", ") + "]");
