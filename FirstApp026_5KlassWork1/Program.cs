deffaed7e9212405b2ff2c0260dfaba2a6e316df
BogdS <Bogdanovas85@gmail.com>
2022-09-30 19:15:36 +0300

    работаВклассе1правка

---
+++ FirstApp026_5KlassWork1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Метод, который умножает число на число и возвращает результат
+// int GetMulti( int a, int b)
+// {
+//     return a * b;
+// }
+
+// //  *******************************
+// // ОСНОВНАЯ ПРОГРАММА
+// int number1 = new Random().Next(0,100);
+// int number2 = new Random().Next(0,100);
+
+// Console.WriteLine(GetMulti(number1,number2));
+// // *************************************
+
+double GetMulti( int a, double b)
+{
+    return a * b;
+}
+
+
+int zp = 5000000;
+double index = 1.1;
+
+Console.WriteLine(GetMulti(zp,index));
+
+
+int kolProd = 10;
+double kolVint = 50;
+
+Console.WriteLine(GetMulti(kolProd,kolVint));
