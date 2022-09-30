062240a1c6c144f3ffd6b2c1353e65021570731d
BogdS <Bogdanovas85@gmail.com>
2022-10-01 00:21:46 +0300

    HomeWork36

---
+++ FirstApp032_5Seminar2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+     Console.Write("[ "); // [ 1, 58, ...]
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+}
+
+int GetSumm1toA(int[] arr)
+{
+    int summ = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 != 0)
+        {
+            summ = summ + arr[i];
+        }
+    }
+    return summ;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+int z = GetSumm1toA(array);
+Console.Write($" -> {z}");
+
