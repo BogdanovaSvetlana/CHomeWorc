3361ceee4e4b57c9e9c82345daf1e00ec432e40f
BogdS <Bogdanovas85@gmail.com>
2022-10-05 00:27:11 +0300

    HomeWorc41

--- FirstApp037_5Seminar1/Program.cs
+++ FirstApp037_5Seminar1/Program.cs
@@ -4,31 +4,43 @@
 // 1, -7, 567, 89, 223-> 3
 int GetNumber(string M)
 {
-    Console.WriteLine($"Укажите, сколько чисел Вы хотите ввести  {M}" );
-    return Convert.ToInt32(Console.ReadLine());
+    int numA = 0;
+    while (numA <= 1)
+    {
+        Console.WriteLine($"Укажите, сколько чисел Вы хотите ввести {M}" );
+        numA = Convert.ToInt32(Console.ReadLine());
+    }
+    return numA;
 }
 
-int r = 0;
-for(int i = 0; i < m; i++)
+string StringArray(int[] array) 
 {
-    Console.WriteLine($"Введите число № {i} " );
+    int lenght = array.Length; 
+    string textarray = String.Empty; 
+    for (int i = 0; i < lenght; i++) 
+    {
+        textarray += array[i]; 
+        if (i < lenght - 1) 
+        {
+            textarray += ", ";
+        }
+    }
+    return textarray; 
+}
+
+int m = GetNumber("M");
+int r = 0;
+int[] array = new int[m];
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine($"Введите число № {i + 1} " );
     int x = Convert.ToInt32(Console.ReadLine());
+    array[i] = x;
     if( x > 0)
     {
         r++;
     }
 }
 
-int Numbers(int[] array)
-{
-    int[] array[i] = 0;
-    for(int i = 0; i < m; i++)
-    {
-        Console.Write(array[i] + "  " -> );
-    }
-    Console.WriteLine();
-}
 
-int m = GetNumber("M");
-Numbers(array);
-Console.WriteLine(r);
+Console.WriteLine($"{StringArray(array)} -> {r}");
