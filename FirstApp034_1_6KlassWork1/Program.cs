acd6232728f326e4c49eeae301eceedcc7f625e9
BogdS <Bogdanovas85@gmail.com>
2022-10-04 11:44:35 +0300

    5работаВклассе1

---
+++ FirstApp034_1_6KlassWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Интерполяция строк
+int a = 5;
+int b = 2;
+
+Console.WriteLine($"Переменные: a={a}, b={b}");
+
+//Рандомное вещественное число от 0 до 100
+double number = new Random().NextDouble();
+double number1 = new Random().NextDouble();
+double number2 = new Random().NextDouble();
+
+Console.WriteLine("Числа: " + Math.Round(number*100,2) + " " + number1*100 + " " + number2*100);
