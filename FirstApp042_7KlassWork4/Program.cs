01ad4568ea7304e757bf11cc737ca7063e500aa7
BogdS <Bogdanovas85@gmail.com>
2022-10-07 19:48:06 +0300

    Work51

---
+++ FirstApp042_7KlassWork4/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+int InputNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Преобразование массива в строку с заданным разделителем
+string StringArray2(int[,] array, string split) 
+{
+    int lenght0 = array.GetLength(0); // получение длины массива
+    int lenght1 = array.GetLength(1); // получение длины массива
+    string textarray = String.Empty; // обнуление строки
+    for( int i = 0; i < lenght0; i++) // цикл по элементам массива
+    {
+        for(int j = 0; j < lenght1; j++)
+        {
+            textarray += array[i, j]; // добавление в строку значения текущего элемента массива
+            if(j < lenght1 - 1) // проверка: является ли элемент последним (нужно ли добавить разделитель в строку)
+            textarray += split; // добавление разделителя между элементами в строку
+        }
+        textarray += "\n";
+    }
+    return textarray; // возврат значения элементов массива в виде полученной строки
+}
+
+void FillArray(int[,] array, int min, int max)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+int DiagonalSum(int[,] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum += array[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+
+int m = InputNumber("Введите первую размерность массива: "),
+    n = InputNumber("Введите вторую размерность массива: "),
+    min = InputNumber("Введите минимальное значение случайных чисел: "),
+    max = InputNumber("Введите максимальное значение случайных чисел: ");
+int[,] array = new int[m, n];
+
+
+FillArray(array, min, max);
+Console.WriteLine(StringArray2(array, ", "));
+Console.WriteLine($"Сумма элементов по диагонали: {DiagonalSum(array)}");
+
+
