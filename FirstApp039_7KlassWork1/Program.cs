a206a5baa01025ff2649ef9128a5001c035aa7a1
BogdS <Bogdanovas85@gmail.com>
2022-10-07 18:20:22 +0300

    Worc46

---
+++ FirstApp039_7KlassWork1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 46: Задайте двумерный массив размерои m*n, 
+// заполненный случайными числами
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 - 2 33 - 2
+// 77 3 8 1
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine("Введите размерность массива " + text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue,  maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+     return new int[m,n];
+}
+
+// КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размер массива m");
+int n = GetNumberFromConsole("Введите размер массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+PrintArray(array);
