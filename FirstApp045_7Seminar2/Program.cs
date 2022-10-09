c810c2379144ac3a69cca04d81e1fadcb6dfbc5c
BogdS <Bogdanovas85@gmail.com>
2022-10-10 01:43:22 +0300

    Work50

---
+++ FirstApp045_7Seminar2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, и возвращает значение
+//  этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+int InputNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Преобразование массива в строку с заданным разделителем
+string StringArray2(int[,] array, string split) 
+{
+    int lenght0 = array.GetLength(0); // получение длины массива
+    int lenght1 = array.GetLength(1); // получение длины массива
+    string textarray = String.Empty; // обнуление строки
+    for( int i = 0; i < lenght0; i++) // цикл по элементам массива
+    {
+        for(int j = 0; j < lenght1; j++)
+        {
+            textarray += array[i, j]; // добавление в строку значения текущего элемента массива
+            if(j < lenght1 - 1) // проверка: является ли элемент последним (нужно ли добавить разделитель в строку)
+            textarray += split; // добавление разделителя между элементами в строку
+        }
+        textarray += "\n";
+    }
+    return textarray; // возврат значения элементов массива в виде полученной строки
+}
+
+void FillArray(int[,] array, int min, int max)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+
+
+int m = InputNumber("Введите первую размерность массива: "),
+    n = InputNumber("Введите вторую размерность массива: "),
+    a = InputNumber("Введите число, для поиска по строке массива: "),
+    b = InputNumber("Введите число, для поиска по столбцу массива: "),
+    min = InputNumber("Введите минимальное значение случайных чисел: "),
+    max = InputNumber("Введите максимальное значение случайных чисел: ");
+int[,] array = new int[m, n];
+
+FillArray(array, min, max);
+Console.WriteLine(StringArray2(array, ", "));
+if(a > m || b > n)
+{
+    Console.WriteLine($"{a},{b} -> Такого числа в массиве нет!");
+}
+else
+{
+    Console.Write($"{a}, {b} -> ");
+    Console.Write(array[a ,b]);
+}
