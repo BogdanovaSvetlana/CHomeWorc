a586d28fe435bdd80e5b41dd421ea3f82abbc639
BogdS <Bogdanovas85@gmail.com>
2022-10-12 14:53:55 +0300

    Work00

---
+++ FirstApp052_8KlassWork2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать рекурсивный метод для деления одного числа на другое.
+//  Нужен остаток и результат деления
+int getNumber(int z)
+{
+    Console.WriteLine($"Введите {z} -е целое число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int divisionRemainder(int x, int y)
+{
+    if(x >= y)
+    {
+        x -= y;
+        return divisionRemainder(x, y);
+    }
+    else
+    {
+        return x;
+    }
+}
+
+int divisionFull(int x, int y)
+{
+    if(x >= y)
+    {
+        x -= y;
+        return divisionFull(x, y) + 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int a = 1;
+int b = 2;
+
+int x = getNumber(a);
+int y = getNumber(b);
+
+Console.WriteLine(
+    $"{x} / {y} = {divisionFull(x, y)} остаток от деления = {divisionRemainder(x, y)}"
+);
+Console.WriteLine();
